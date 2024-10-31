371d99bb397d2bd941c6e7b0cb5e34e52816e6ef
cprb2n@umsystem.edu <cprb2n@umsystem.edu>
2024-10-30 19:48:34 -0500

    Update shared_functions.py

--- Backend/shared_functions.py
+++ Backend/shared_functions.py
@@ -15,6 +15,7 @@ def get_driver() -> webdriver:
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
+    options.binary_location = "/usr/bin/google-chrome"
     driver = webdriver.Chrome(options=options)
 
     # driver = webdriver.Chrome()
